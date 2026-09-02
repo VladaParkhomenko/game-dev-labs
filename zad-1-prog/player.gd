1715aa78d0f7c7db8c460e27f1c581a78e1870de
VladaParkhomenko <252503@edu.p.lodz.pl>
2026-09-02 14:40:43 +0200

    feat: add player movement script and controls

---
+++ zad-1-prog/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var speed: float = 200.0
+
+func _physics_process(delta: float) -> void:
+	#WASD
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, speed)
+	
+	move_and_slide()
