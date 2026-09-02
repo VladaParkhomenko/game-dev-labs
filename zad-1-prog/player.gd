70a731422c6965bc1dcb72d314851d7fc98bd069
VladaParkhomenko <252503@edu.p.lodz.pl>
2026-09-02 15:10:48 +0200

    feat: add static obstacles and wall collisions

--- zad-1-prog/player.gd
+++ zad-1-prog/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var speed: float = 200.0
 
 func _physics_process(delta: float) -> void:
-	#WASD
+	#WASD i strzałki
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if direction:
