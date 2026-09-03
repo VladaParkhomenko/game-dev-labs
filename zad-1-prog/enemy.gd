079988bf68105786d2291b8e98b28ac54b9aa212
VladaParkhomenko <252503@edu.p.lodz.pl>
2026-09-03 17:10:30 +0200

    feat: fix sprite sheet slicing and scale up player and enemy animations

--- zad-1-prog/enemy.gd
+++ zad-1-prog/enemy.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 @export var speed: float = 140.0
 @onready var player: CharacterBody2D = get_node("../Player")
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if player:
-		#direction to Player
 		var direction = global_position.direction_to(player.global_position)
 		velocity = direction * speed
-
+		animated_sprite.play("run")
+		if direction.x < 0:
+			animated_sprite.flip_h = true
+		elif direction.x > 0:
+			animated_sprite.flip_h = false
+			
 		move_and_slide()
--- zad-1-prog/player.gd
+++ zad-1-prog/player.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	#WASD i strzałki
+	#WASD
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if direction:
 		velocity = direction * speed
+		animated_sprite.play("run")
+		if direction.x < 0:
+			animated_sprite.flip_h = true  
+		elif direction.x > 0:
+			animated_sprite.flip_h = false 
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, speed)
-	
+		animated_sprite.play("idle")
+		
 	move_and_slide()
