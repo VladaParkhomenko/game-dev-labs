fca502a880c79d3fe73d068071b435130f868363
VladaParkhomenko <252503@edu.p.lodz.pl>
2026-09-12 19:11:21 +0200

    update enemy script to handle movement and obstacle avoidance without A-star

--- zad-1-prog/enemy.gd
+++ zad-1-prog/enemy.gd
@@ -1,17 +1,22 @@
 extends CharacterBody2D
-
 @export var speed: float = 140.0
 @onready var player: CharacterBody2D = get_node("../Player")
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+var slide_timer: float = 0.0
 func _physics_process(delta: float) -> void:
 	if player:
-		var direction = global_position.direction_to(player.global_position)
-		velocity = direction * speed
+		var target_direction = global_position.direction_to(player.global_position)
+		if slide_timer > 0:
+			slide_timer -= delta
+			var orthogonal = Vector2(-target_direction.y, target_direction.x)
+			velocity = (target_direction + orthogonal * 1.5).normalized() * speed
+		else:
+			velocity = target_direction * speed
 		animated_sprite.play("run")
-		if direction.x < 0:
+		if target_direction.x < 0:
 			animated_sprite.flip_h = true
-		elif direction.x > 0:
+		elif target_direction.x > 0:
 			animated_sprite.flip_h = false
-			
 		move_and_slide()
+		if get_slide_collision_count() > 0 and velocity.length() < 10.0:
+			slide_timer = 0.5 
