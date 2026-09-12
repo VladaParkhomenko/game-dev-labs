120658bd39d881d2f991e9dc1cf2c537ce080674
VladaParkhomenko <252503@edu.p.lodz.pl>
2026-09-12 19:11:00 +0200

    update player script with AnimationPlayer synchronization for dual collision states

--- zad-1-prog/player.gd
+++ zad-1-prog/player.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
-
 @export var speed: float = 200.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 func _physics_process(delta: float) -> void:
 	#WASD
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	if direction:
+
+	if direction != Vector2.ZERO:
 		velocity = direction * speed
 		animated_sprite.play("run")
+		animation_player.play("run_anim") 
 		if direction.x < 0:
 			animated_sprite.flip_h = true  
 		elif direction.x > 0:
@@ -17,5 +17,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, speed)
 		animated_sprite.play("idle")
-		
+		animation_player.play("idle_anim")
 	move_and_slide()
